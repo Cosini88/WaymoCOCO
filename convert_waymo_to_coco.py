65849fd342ffe58931afad34f6601f5e7f87d5e2
Cosini88 <bock.lilla@gmail.com>
2022-06-12 00:16:28 +0200

    Changed for my project

--- convert_waymo_to_coco.py
+++ convert_waymo_to_coco.py
@@ -3,6 +3,9 @@ import json
 import pathlib
 import argparse
 import datetime
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from PIL import Image
 
 import tensorflow as tf
 
@@ -69,8 +72,8 @@ class WaymoCOCOConverter():
 
     def init_coco_format_info(self):
         self.dataset_info = {
-            "year": 2020,
-            "version": "v1.2_20200409",
+            "year": 2022,
+            "version": "v1.3.1_20220422",
             "description": "Waymo Open Dataset 2D Detection",
             "contributor": "Waymo LLC",
             "url": "https://waymo.com/open/",
@@ -87,24 +90,24 @@ class WaymoCOCOConverter():
         self.target_categories = [
             {
                 "id": 1,
-                "name": "TYPE_VEHICLE",
-                "supercategory": "vehicle",
-            },
-            {
-                "id": 2,
                 "name": "TYPE_PEDESTRIAN",
                 "supercategory": "person",
             },
             {
-                "id": 3,
+                "id": 2,
                 "name": "TYPE_CYCLIST",
                 "supercategory": "bike_plus",
             },
+            {
+                "id": 3,
+                "name": "TYPE_VEHICLE",
+                "supercategory": "vehicle",
+            }
             # {
-            #     "id" : ,
+            #     "id" : 13,
             #     "name" : "TYPE_SIGN",
             #     "supercategory" : "outdoor",
-            # },
+            # }
             # {
             #     "id" : ,
             #     "name" : "TYPE_UNKNOWN",
@@ -130,32 +133,70 @@ class WaymoCOCOConverter():
                 self.process_frame(frame, frame_index, tfrecord_index)
 
     def process_frame(self, frame, frame_index, tfrecord_index):
-        print(f'frame: {frame_index}, {len(frame.images)} cameras')
+        #print(f'frame: {frame_index}, {len(frame.images)} cameras')
 
         # loop for camera images in each frame
         for camera_image in frame.images:
+            if camera_image.name !=1:
+                break
             self.process_img(camera_image, frame, frame_index, tfrecord_index)
+            #print(frame.camera_labels)
+            img = tf.image.decode_jpeg(camera_image.image).numpy()[:, :, ::-1]
+            img_height = img.shape[0]
+            img_width = img.shape[1]
+            img_size = img_height * img_width
             for camera_label in frame.camera_labels:
                 # Ignore camera labels that do not correspond to this camera.
                 if camera_label.name != camera_image.name:
                     continue
-                self.add_coco_annotation_dict(camera_label)
+                self.add_coco_annotation_dict(camera_label, img_size)
             # increment img_index after adding annotations
             self.img_index += 1
 
     def process_img(self, camera_image, frame, frame_index, tfrecord_index):
         img_filename = f'{tfrecord_index:05d}_{frame_index:05d}_camera{camera_image.name}.jpg'  # noqa
+        # imgbox_filename = f'{tfrecord_index:05d}_{frame_index:05d}_camera{camera_image.name}_waymo_boxes.jpg'
         # img_filename = f'{frame.context.name}_{camera_image.name}_{frame.timestamp_micros}.jpg'  # noqa
         if self.image_prefix is not None:
             img_filename = self.image_prefix + '_' + img_filename
         img_path = os.path.join(self.image_dir, img_filename)
-
         img = tf.image.decode_jpeg(camera_image.image).numpy()[:, :, ::-1]
         img_height = img.shape[0]
         img_width = img.shape[1]
+        img_size = img_height*img_width
         if self.write_image:
             with open(img_path, 'wb') as f:
                 f.write(bytearray(camera_image.image))
+        # Draw the camera labels.
+        # for camera_labels in frame.camera_labels:
+        #     # Ignore camera labels that do not correspond to this camera.
+        #     ax = plt.subplot()
+        #     ax.clear()
+        #     if camera_labels.name != camera_image.name:
+        #         continue
+        #
+        #     # Iterate over the individual labels.
+        #     else:
+        #         for label in camera_labels.labels:
+        #         # Draw the object bounding box.
+        #             if label.box.length*label.box.width<0.001*img_size:
+        #                 continue
+        #             ax.add_patch(patches.Rectangle(
+        #                xy=(label.box.center_x - 0.5 * label.box.length,
+        #                    label.box.center_y - 0.5 * label.box.width),
+        #                width=label.box.length,
+        #                height=label.box.width,
+        #                linewidth=1,
+        #                edgecolor='red',
+        #                facecolor='none'))
+
+            # Show the camera image.
+            # plt.imshow(tf.image.decode_jpeg(camera_image.image))
+            # plt.title(open_dataset.CameraName.Name.Name(camera_image.name))
+            # plt.grid(False)
+            # plt.axis('off')
+            # img_path2 = os.path.join(self.image_dir, 'waymo_boxes', imgbox_filename)
+            # plt.savefig(img_path2)
 
         self.add_coco_img_dict(img_filename,
                                height=img_height,
@@ -198,14 +239,17 @@ class WaymoCOCOConverter():
 
         self.img_dicts.append(img_dict)
 
-    def add_coco_annotation_dict(self, camera_label):
+    def add_coco_annotation_dict(self, camera_label, img_size):
         annotation_dicts = []
         for box_label in camera_label.labels:
+ #           print(box_label)
             category_name_to_id = {
                 category['name']: category['id']
                 for category in self.target_categories
             }
             category_name = self.waymo_class_mapping[box_label.type]
+            if category_name not in category_name_to_id:
+                continue
             category_id = category_name_to_id[category_name]
             width = box_label.box.length  # box.length: dim x
             height = box_label.box.width  # box.width: dim y
@@ -227,6 +271,8 @@ class WaymoCOCOConverter():
                     box_label.detection_difficulty_level
                 annotation_dict["track_difficult"] = \
                     box_label.tracking_difficulty_level
+            if annotation_dict["area"] <0.005*img_size:
+                continue
             annotation_dicts.append(annotation_dict)
             self.annotation_index += 1
         self.annotation_dicts.extend(annotation_dicts)
@@ -250,6 +296,7 @@ class WaymoCOCOConverter():
                       f,
                       indent=json_indent,
                       sort_keys=False)
+        return output_dict
 
 
 def main():
@@ -292,7 +339,35 @@ def main():
         add_waymo_info=args.add_waymo_info,
         frame_index_ones_place=args.frame_index_ones_place)
     waymo_converter.process_sequences(tfrecord_list)
-    waymo_converter.write_coco(label_path, json_indent=args.json_indent)
+    out_dict = waymo_converter.write_coco(label_path, json_indent=args.json_indent)
+    # print(f'Output dictionary: {out_dict}')
+    # f = open(label_path)
+    # out_dict = json.load(f)
+    # annot = out_dict['annotations']
+    # ims = out_dict['images']
+    # path = os.path.join(image_dir, 'coco_boxes')
+    # os.makedirs(path)
+    # for j in ims:
+    #     img_path1 = os.path.join(image_dir, j['file_name'])
+    #     img = Image.open(img_path1)
+    #     ax = plt.gca()
+    #     ax.clear()
+    #     plt.imshow(img)
+    #     n = j['id']
+    #     for i in annot:
+    #         if i['image_id'] == n:
+    #             x1 = i['bbox'][0]
+    #             y1 = i['bbox'][1]
+    #             w = i['bbox'][2]
+    #             h = i['bbox'][3]
+    #             rect = patches.Rectangle((x1, y1), w, h, linewidth=1, edgecolor='r', facecolor='none')
+    #             ax.add_patch(rect)
+    #         else:
+    #             continue
+    #     img_path2 = os.path.join(path, j['file_name'])
+    #     plt.savefig(img_path2)
+    # f.close()
+
 
 
 if __name__ == "__main__":
